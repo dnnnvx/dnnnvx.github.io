d0a14e7b313b2cefb41ff1f9604545c4099f4487
dnnnvx <dotmarcodestefani@gmail.com>
2024-12-12 16:01:59 +0100

    update base path

--- svelte.config.js
+++ svelte.config.js
@@ -15,7 +15,7 @@ const config = {
       strict: true,
     }),
     paths: {
-      base: process.argv.includes("dev") ? "" : process.env.BASE_PATH,
+      base: process.argv.includes("dev") ? "" : "/",
     },
     version: {
       // pollInterval: 100000,
